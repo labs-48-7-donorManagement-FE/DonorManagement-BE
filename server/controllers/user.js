783a378d1f7b5af6cee6ef47488566a69d944041
Soji Soyoye <soji.soyoye@gmail.com>
2019-06-25 19:13:47 +0100

    crud operations for app

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,60 +4,69 @@ import { tokenGenerator, comparePassword } from '../helpers/utils';
 const { User } = model;
 
 export const signUp = async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-    
-    const userData = {
-        firstName,
-        lastName,
-        email,
-        password
-    };
+  const { firstName, lastName, email, password } = req.body;
 
-    try {
-        const newUser = await User.create(userData);
-        const { id } = newUser;
-        const token = tokenGenerator({ id });
+  const userData = {
+    firstName,
+    lastName,
+    email,
+    password
+  };
 
-       return res.status(200).json({
-            status: 'success',
-            message: 'User created successfully',
-            data: newUser,
-            token
-        });
-    } catch (error) {
-        console.log(error);
+  try {
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res.status(409).json({
+        status: 'failed',
+        message: 'User with this email already exist'
+      });
     }
-}
+
+    const newUser = await User.create(userData);
+    const { id } = newUser;
+    const token = tokenGenerator({ id });
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'User created successfully',
+      data: newUser,
+      token
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const user = await User.findOne({ where: { email } });
+  try {
+    const user = await User.findOne({ where: { email } });
 
-        if (!user) {
-            return res.status(404).json({
-                status: 'failed',
-                messaege: 'User does not exist'
-            })
-         } 
+    if (!user) {
+      return res.status(404).json({
+        status: 'failed',
+        messaege: 'User does not exist'
+      });
+    }
 
-        const { id } = user
-        const token = tokenGenerator({ id });
+    const { id } = user;
+    const token = tokenGenerator({ id });
 
     if (user && !comparePassword(user.password, password)) {
       res.status(400).json({
-          status: 'failed',
-          message: 'Incorrect password'
-      })  
+        status: 'failed',
+        message: 'Incorrect password'
+      });
     } else {
-       res.status(200).json({
-           status: 'success',
-           message: 'User login successful',
-           token
-       })
-   }
-    } catch (error) {
-        console.log(error);
+      res.status(200).json({
+        status: 'success',
+        message: 'User login successful',
+        token
+      });
     }
-} 
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import { signUp, login } from '../controllers/user';
+import { createDonor, getAllDonors, updateDonor } from '../controllers/donor';
 import validateSignUp from '../middlewares/validations/signup';
 import validateLogin from '../middlewares/validations/login';
 
 const router = express.Router();
 
+/* User Routes */
 router.post('/auth/signup', validateSignUp, signUp);
 router.post('/auth/login', validateLogin, login);
+
+/* Donor Routes */ 
+router.post('/donors', createDonor);
+router.get('/donors', getAllDonors);
+router.patch('/donors/:id', updateDonor);
 
 export default router;
